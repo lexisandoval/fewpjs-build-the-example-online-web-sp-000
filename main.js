70ad7aad76ad674184161fc3bcdc865423e10cc4
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-04-09 12:29:50 -0500

    commit tree 96480d6dc402a4a7b5fcddc2fb2a75f37aa9ed0a

--- main.js
+++ main.js
@@ -3,7 +3,35 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let hearts = document.querySelectorAll(".like-glyph");
 
+let type = {
+  '♥' : '♡',
+  '♡' : '♥'
+}
+let color = {
+  "red" : "",
+  "" : "red"
+}
+
+const errorMessage = document.getElementById("modal");
+errorMessage.classList.add("hidden");
+
+hearts.forEach(heart => heart.addEventListener("click", changeHeart));
+
+function changeHeart(event){
+  let heart = event.target
+  mimicServerCall("You sure?")
+  .then(function(error){
+    heart.innerText = type[heart.innerText];
+    heart.style.color = color[heart.style.color];
+  })
+  .catch(function(error){
+    errorMessage.innerText = "Something went wrong";
+    errorMessage.className = "";
+    setTimeout(function(){errorMessage.classList.add("hidden");}, 4000)
+  })
+}
 
 
 
